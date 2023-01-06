75e152a28ef2be06373f35ea6bd4361e27387a3e
Gopakumar R <Gopakumar.R@live.com>
2023-01-07 10:18:37 +1100

    Updated the loading synbol

--- KidsLearn/Client/wwwroot/js/app.js
+++ KidsLearn/Client/wwwroot/js/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext("2d");
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+context.canvas.width = width;
+context.canvas.height = height;
+var page = document.getElementById("pageFlip");
+
+//page.play();
+/*page.loop = true;*/
+$('#myCanvas').fadeTo(8000, 0.0, function () {
+    //page.loop = false;
+    /*page.stop();*/
+});
